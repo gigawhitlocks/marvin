e43d2db8ad5d0aa8b2085d52d1dbfe4817dfddf8
Ian Whitlock <ian@theknown.net>
2016-05-27 21:38:44 -0500

    in Go this time

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"flag"
-	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -12,19 +12,29 @@ var (
 	password string
 	username string
 	server   string
-
-	userId    string
-	authToken string
 )
 
 type logonData struct {
-	AuthToken string
 	UserId    string
+	AuthToken string
+}
+
+type result struct {
+	Status string
 }
 
 type logonResponse struct {
 	Status string
 	Data   logonData
+}
+
+type Session struct {
+	logonData
+	server string
+}
+
+type Message struct {
+	Contents string `json:"msg"`
 }
 
 func init() {
@@ -34,7 +44,9 @@ func init() {
 	flag.Parse()
 }
 
-func logon() (string, string) {
+func (s *Session) Logon(username, password, server string) {
+	s.server = server
+
 	b := new(bytes.Buffer)
 
 	b.WriteString("user=")
@@ -52,15 +64,70 @@ func logon() (string, string) {
 		panic("Probably not the right username/password/server")
 	}
 
-	return l.Data.UserId, l.Data.AuthToken
+	s.UserId = l.Data.UserId
+	s.AuthToken = l.Data.AuthToken
 }
 
-func joinChannel(channelName string) {
+func (s *Session) authRequest(url string) (*http.Request, *http.Client) {
+	client := &http.Client{}
+	b := new(bytes.Buffer)
+	req, _ := http.NewRequest("POST", url, b)
+
+	req.Header.Add("X-Auth-Token", s.AuthToken)
+	req.Header.Add("X-User-Id", s.UserId)
+
+	return req, client
+}
+
+func checkResult(body io.Reader) {
+	r := &result{}
+	err := json.NewDecoder(body).Decode(&r)
+
+	if err != nil {
+		panic(err)
+	}
+	if r.Status != "success" {
+		panic(r.Status)
+	}
+}
+
+func (s *Session) JoinChannel(channelName string) {
+	req, client := s.authRequest(s.server + "/api/rooms/" + channelName + "/join")
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	checkResult(resp.Body)
+}
+
+func (s *Session) Say(channelName, message string) {
+	client := &http.Client{}
+	b := new(bytes.Buffer)
+	b.WriteString("{\"msg\": \"" + message + "\"}")
+	req, _ := http.NewRequest("POST",
+		s.server+"/api/rooms/"+channelName+"/send", b)
+
+	req.Header.Add("X-Auth-Token", s.AuthToken)
+	req.Header.Add("X-User-Id", s.UserId)
+	req.Header.Add("Content-Type", "application/json")
+
+	client.Do(req)
+}
+
+func (s *Session) LeaveChannel(channelName string) {
+	req, client := s.authRequest(s.server + "/api/rooms/" + channelName + "/leave")
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	checkResult(resp.Body)
 
 }
 
 func main() {
-	userId, authToken := logon()
-	fmt.Printf("%s %s", userId, authToken)
-	joinChannel("general")
+	s := &Session{}
+	s.Logon(username, password, server)
+	s.JoinChannel("GENERAL")
+	s.Say("GENERAL", "test")
+	s.LeaveChannel("GENERAL")
 }
