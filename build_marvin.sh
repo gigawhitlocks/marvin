a48677f10dd9c895dacfef57bc8f81faed223dce
Ian Whitlock <ian@theknown.net>
2016-05-27 22:34:41 -0500

    reorg

---
+++ build_marvin.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+go build -o bin/marvin && chmod +x bin/marvin
---
+++ run_marvin.sh
@@ -0,0 +1,1 @@
+"$GOPATH"/src/github.com/gigawhitlocks/marvin/bin/marvin -username="marvin" -password="$(echo "$MARVINPW")" -server="https://shouting.online"
